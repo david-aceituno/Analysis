7ca0fb9aab6182c437bf96a22e79484497086102
David Aceituno <daceituno@gmail.com>
2025-11-18 17:19:04 +0000

    Add log-transformed PSA covariate

--- scripts/maic_VISION.R
+++ scripts/maic_VISION.R
@@ -21,10 +21,11 @@ comparator_ipd$PFS <- read_csv(here(km_directory, "PFS_VISION_PLU.csv"))
 
 # Load comparator baseline data
 comparator_baseline <- as.data.frame(read_xlsx(here("data", "baseline_VISION.xlsx"))) #note: ECOG in VISION include ECOG 0-1
+comparator_baseline <- comparator_baseline |> mutate(psa_log = log(psa))
 
 # 3. Prepare data for MAIC --------------------------------------------------------
 outcome_names <- c("OS", "PFS")
-matching_variables <- c("age", "ecog_0", "gleason_8", "psa")
+matching_variables <- c("age", "ecog", "gleason_8", "psa_log")
 
 # Comparator name
 comparator_names <- c("Pluvicto")
@@ -56,35 +57,36 @@ for(outcome_name in outcome_names) {
   maic_ipd[[outcome_name]]  <- maic_ipd[[outcome_name]][, c("treatment", "time", "event", matching_variables)]
   
   # Harmonise names and change time to years
-  colnames(maic_ipd[[outcome_name]]) <- c("TRT01P", "time", "event", matching_variables)
-  maic_ipd[[outcome_name]]$time <- as.numeric(maic_ipd[[outcome_name]]$time) / 365.25
+  #colnames(maic_ipd[[outcome_name]]) <- c("treatment", "time", "event", matching_variables)
+  #maic_ipd[[outcome_name]]$time <- as.numeric(maic_ipd[[outcome_name]]$time) / 365.25
   
   # Combine with comparator data
-  temp <- as.data.frame(comparator_ipd[[outcome_name]][[comparator_study]][[comparator_name]])
+  temp <- as.data.frame(comparator_ipd[[outcome_name]])
   temp <- temp[, c("time", "event")]
-  temp$TRT01P <- comparator_name
+  temp$treatment <- comparator_name
+  
   # Add empty data for the matching characteristics
   for(i in 1:length(matching_variables)) temp[[matching_variables[i]]] <- NA
   
   # Change time from months to years
   temp$time <- as.numeric(temp$time) / 12
-  temp <- temp[, c("TRT01P", "time", "event", matching_variables)]
+  temp <- temp[, c("treatment", "time", "event", matching_variables)]
   maic_ipd[[outcome_name]] <- rbind(maic_ipd[[outcome_name]], temp)
   
   # Naive survival analyses
-  km_fit[[outcome_name]] <- survfit(Surv(time, event) ~ TRT01P, data = maic_ipd[[outcome_name]])
-  cox_fit[[outcome_name]] <- coxph(Surv(time, event) ~ TRT01P, data = maic_ipd[[outcome_name]])
+  km_fit[[outcome_name]] <- survfit(Surv(time, event) ~ treatment, data = maic_ipd[[outcome_name]])
+  cox_fit[[outcome_name]] <- coxph(Surv(time, event) ~ treatment, data = maic_ipd[[outcome_name]])
   
   # KM plot
   
   # MAIC Reweighting
-  # Number of regorafenib patients in peace3
-  n_ipd_patients <- sum(maic_ipd[[outcome_name]]$TRT01P == "Regorafenib 160 mg") 
+  # Number of Ra223+ENZ patients in peace3
+  n_ipd_patients <- sum(maic_ipd[[outcome_name]]$treatment == "Radium-223 + Enzalutamide") 
   # Matching variables in peace3
-  x_ipd <- maic_ipd[[outcome_name]][maic_ipd[[outcome_name]]$TRT01P == "Regorafenib 160 mg",c(matching_variables)] 
+  x_ipd <- maic_ipd[[outcome_name]][maic_ipd[[outcome_name]]$treatment == "Radium-223 + Enzalutamide",c(matching_variables)] 
   # Average matching variables in comparator
-  x_comparator <- comparator_baseline[comparator_baseline$Study == comparator_study & 
-                                        comparator_baseline$Treatment == comparator_name,
+  x_comparator <- comparator_baseline[comparator_baseline$study == comparator_study & 
+                                        comparator_baseline$trt == comparator_name,
                                       c(matching_variables)] 
   n_matching <- length(matching_variables)
   
@@ -96,9 +98,9 @@ for(outcome_name in outcome_names) {
     if(sum(is.na(x_ipd[, k])) > 0) x_ipd[is.na(x_ipd[, k]), k] <- mean(x_ipd[, k], na.rm = TRUE)
   }
   
-  
   # center the IPD matching variables on comparator means but keep an uncentered version
   x_ipd_uncentered <- x_ipd
+  
   # Requires all variables to be numeric
   for (k in 1:n_matching) {
     x_ipd[,k] <- x_ipd[,k] - x_comparator[,k]
@@ -112,67 +114,69 @@ for(outcome_name in outcome_names) {
   alpha <- rep(1, n_matching) # arbitrary starting point for the optimiser
   
   # objective function minimized using BFGS
-  Q_min <- optim(fn = Q, x_ipd = as.matrix(x_ipd), par=alpha , method ="BFGS")
+  Q_min <- optim(fn = Q, x_ipd = as.matrix(x_ipd), par=alpha, method ="BFGS")
   
   hat_alpha <- Q_min$par # finite solution is the logistic regression parameters
   log_hat_w <- rep(0, n_ipd_patients)
   for (k in 1:n_matching) {
     log_hat_w <- log_hat_w + hat_alpha[k] * x_ipd[,k]
   }
+  
   # Estimated weights
   hat_w <- exp (log_hat_w) 
+  
   # Estimated effective sample size
   ess <- sum (hat_w)^2 / sum (hat_w^2)
   
   baseline_table <- matrix(nrow = 3, ncol = n_matching)
-  rownames(baseline_table) <- c(comparator_name, "Regorafenib unweighted", "Regorafenib weighted")
+  rownames(baseline_table) <- c(comparator_name, "Ra223+ENZ unweighted", "Ra223+ENZ weighted")
   colnames(baseline_table) <- matching_variables
   
   # Comparator means
   baseline_table[comparator_name, c(1:n_matching)] <- as.matrix(x_comparator)
-  # Unweighted means
-  baseline_table["Regorafenib unweighted", ] <- colMeans(x_ipd_uncentered)
-  # Weighted means
-  baseline_table["Regorafenib weighted", ] <- (hat_w %*% x_ipd_uncentered) / sum(hat_w)
-  # Fix Sex variable to match publications
-  baseline_table[,2] <- 1-baseline_table[,2]
   
-  write.csv(baseline_table, here("report", paste0("baseline_table_", comparator_name, ".csv")))
+  # Unweighted means
+  baseline_table["Ra223+ENZ unweighted", ] <- colMeans(x_ipd_uncentered)
+  
+  # Weighted means
+  baseline_table["Ra223+ENZ weighted", ] <- (hat_w %*% x_ipd_uncentered) / sum(hat_w)
+  
+  write.csv(baseline_table, here("results", paste0("baseline_table_", comparator_name, ".csv")))
   
   # Export a histogram of patient weights
-  jpeg(here("report", paste0("weights_histogram_", comparator_name, "_", outcome_name, ".jpg")))
+  jpeg(here("results", paste0("weights_histogram_", comparator_name, "_", outcome_name, ".jpg")))
   hist(hat_w, breaks = 20, main = "Histogram of patient weights", xlab = "")
   dev.off()
   
   # Append weights = 1 for each comparator patient so they're unaffected
-  maic_weights <- c(hat_w, rep(1, sum(maic_ipd[[outcome_name]]$TRT01P == comparator_name)))
+  maic_weights <- c(hat_w, rep(1, sum(maic_ipd[[outcome_name]]$treatment == comparator_name)))
   
   # MAIC weighted survival analysis
-  maic_cox_fit[[outcome_name]] <- coxph ( Surv(time, event) ~ TRT01P,
+  maic_cox_fit[[outcome_name]] <- coxph(Surv(time, event) ~ treatment,
                                           robust = TRUE , weights = maic_weights, 
                                           data = maic_ipd[[outcome_name]] )
   
-  maic_km_fit[[outcome_name]] <- survfit(Surv(time, event) ~ TRT01P, weights = maic_weights,
+  maic_km_fit[[outcome_name]] <- survfit(Surv(time, event) ~ treatment, weights = maic_weights,
                                          data = maic_ipd[[outcome_name]])
   
   # Summarise the results with KM curves
   # KM unweighted (naive)
-  jpeg(here("report", paste0("naive_km_", comparator_name, "_", outcome_name, ".jpg")))
+  jpeg(here("results", paste0("naive_km_", comparator_name, "_", outcome_name, ".jpg")))
   print(ggsurvplot(km_fit[[outcome_name]], data = maic_ipd[[outcome_name]], risk.table = TRUE, risk.table.height = 0.35, legend.labs =
-                     c("Atezolizumab+bevacizumab", "Regorafenib 160 mg"), xlab="Time in years", ylab = outcome_name,
+                     c("Pluvicto", "Radium-223 + Enzalutamide"), xlab="Time in years", ylab = outcome_name,
                    linetype = c(1:2)))
   dev.off()
   
   # KM weighed by MAIC
-  jpeg(here("report", paste0("maic_km_", comparator_name, "_", outcome_name, ".jpg")))
+  jpeg(here("results", paste0("maic_km_", comparator_name, "_", outcome_name, ".jpg")))
   print(ggsurvplot(maic_km_fit[[outcome_name]], data = maic_ipd[[outcome_name]], risk.table = TRUE, risk.table.height = 0.35, legend.labs =
-                     c("Atezolizumab+bevacizumab", "Regorafenib 160 mg"), xlab="Time in years", ylab = outcome_name,
+                     c("Pluvicto", "Radium-223 + Enzalutamide"), xlab="Time in years", ylab = outcome_name,
                    linetype = c(1:2)))
   dev.off()
   
   # Summarise results in a table
   results_table[paste(outcome_name, comparator_name), "ESS"] <- format(ess, digits = 5)
-  results_table[paste(outcome_name, comparator_name), "Naive median survival Regorafenib"] <-
+  results_table[paste(outcome_name, comparator_name), "Naive median survival Ra223+ENZ"] <-
     format_continuous_result(12 * surv_median(km_fit[[outcome_name]])[2, c("median", "lower", "upper")])
   results_table[paste(outcome_name, comparator_name), "Naive median survival Comparator"] <-
     format_continuous_result(12 * surv_median(km_fit[[outcome_name]])[1, c("median", "lower", "upper")])
@@ -181,7 +185,7 @@ for(outcome_name in outcome_names) {
     format_continuous_result(c(summary(cox_fit[[outcome_name]])$conf.int[1, c("exp(coef)", "lower .95", "upper .95")],
                                summary(cox_fit[[outcome_name]])$logtest["pvalue"]))
   
-  results_table[paste(outcome_name, comparator_name), "MAIC median survival Regorafenib"] <-
+  results_table[paste(outcome_name, comparator_name), "MAIC median survival Ra223+ENZ"] <-
     format_continuous_result(12 * surv_median(maic_km_fit[[outcome_name]])[2, c("median", "lower", "upper")])
   results_table[paste(outcome_name, comparator_name), "MAIC median survival Comparator"] <-
     format_continuous_result(12 * surv_median(maic_km_fit[[outcome_name]])[1, c("median", "lower", "upper")])
@@ -193,4 +197,4 @@ for(outcome_name in outcome_names) {
 }
 
 # 5. Export results --------------------------------------------------------
-write.csv(results_table, here("report", "maic_results_VISION.csv"))  
+write.csv(results_table, here("results", "maic_results_VISION.csv"))  
